2db9426bb943a430312183b7e7943d9cad3408e4
Matheus Felipe Rodrigues Costa <matheuz_2001@hotmail.com>
2024-07-29 10:57:35 -0300

    Terceira atividade em processo

--- Programação de computadores/Lista 2/atividade-3.py
+++ Programação de computadores/Lista 2/atividade-3.py
@@ -1,23 +1,9 @@
 n = int(input("Digite a quantia de primos"))
-count = 0
-num = 2  #Primeiro primo
-
-while count < n:
+for i in range(2, n):
     primo = True
-    if num <= 1:
-        primo = False
-    elif num <= 3:
-        primo = True
-    elif num % 2 == 0 or num % 3 == 0:
-        primo = False
-    else:
-        i = 5
-        while i * i <= num:
-            if num % i == 0 or num % (i + 2) == 0:
-                primo = False
-                break
-            i += 6
+    for j in range(2, i // 2 + 1):
+        if i % j == 0:
+            primo = False
+            break
     if primo:
-        print(num)
-        count += 1
-    num += 1
+        print(f"{i} é um número primo")
