1aafa08e2e75fdfa374d2420d9e73ce2ef4b4b73
Matheus Felipe Rodrigues Costa <matheuz_2001@hotmail.com>
2024-08-02 19:53:49 -0300

    Atividade 4

--- Programação de computadores/Lista 1/atividade-1.py
+++ Programação de computadores/Lista 1/atividade-1.py
@@ -2,10 +2,10 @@
 import sys
 x = int(input("Informe um número inteiro de até 4 digitos: "))
 if(x >= 0 and x <= 9999):
-    #print(x//1000)               algarismo 1
-    #print(x//100-((x//1000)*10)) algarismo 2
-    #print(x//10-((x//100)*10))   algarismo 3
-    #print(x - ((x//10)*10))      algarismo 4
+    print(x//1000)               #algarismo 1
+    print(x//100-((x//1000)*10)) #algarismo 2
+    print(x//10-((x//100)*10))   #algarismo 3
+    print(x - ((x//10)*10))      #algarismo 4
     print(f'A soma dos algarismos é: {(x//1000)+ (x//100-((x//1000)*10)) + (x//10-((x//100)*10)) + (x - ((x//10)*10))}')
     sys.exit()
 print('Número invalido')
