94f7933c7d1c3c146acc85cb013528d7a9a6ec52
Matheus Felipe Rodrigues Costa <matheuz_2001@hotmail.com>
2024-08-28 10:53:54 -0300

    atividade 8 revisada

--- Programação de computadores/Lista 3/atividade-8.py
+++ Programação de computadores/Lista 3/atividade-8.py
@@ -8,13 +8,19 @@ strCMD = 'tracert -d4 ' + destino
 caminho = subprocess.run (strCMD, capture_output=True).stdout.decode('latin1')
 caminho = caminho.split('\r\n')
 caminho = [i.split() for i in caminho]
+pings = list()
 for x in caminho:
-    print(x)
-salvarArquivo.write(caminho)
+    if(len(x)>6):
+        pings.append([x[0],x[1],x[3],x[5],x[7]])
+        #print(x)
+        #print(x[0],x[1],x[3],x[5],x[7])
+#pings.sort(key=lambda x: x[1],reverse=True)
+pings.sort(key=lambda x: x[1])
+print(pings)
+for x in pings:
+    if x[4] == 'limite':
+        pings.pop(0)
+print(pings)
+for x in pings:
+    salvarArquivo.write(f'{x}\n')
 salvarArquivo.close()
-'''
-lerArquivo = open(dirArquivos + '\\rastreio.txt','r',encoding='utf-8')
-saltos = [i.split('\r\n') for i in lerArquivo]
-print(saltos)
-'''
-#'\r\n'
