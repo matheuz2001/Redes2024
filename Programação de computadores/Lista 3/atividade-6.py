2d0bb08dbe143f13a05f8ea82ba464d0b74db280
Matheus Felipe Rodrigues Costa <matheuz_2001@hotmail.com>
2024-08-28 19:29:04 -0300

    Ultimas alterações

--- Programação de computadores/Lista 3/atividade-6.py
+++ Programação de computadores/Lista 3/atividade-6.py
@@ -6,6 +6,7 @@ lerArquivo = open(dirArquivos + '\\CotacoesDolar2023.csv','r',encoding='utf-8')
 arquivo =list()
 cotaçãomedia = [['Janeiro'],['Fevereiro'],['Março'],['Abril'],['Maio'],['Junho'],['Julho'],['Agosto'],['Setembro'],['Outubro'],['Novembro'],['Dezembro']]
 cotaçãomax = [['Janeiro'],['Fevereiro'],['Março'],['Abril'],['Maio'],['Junho'],['Julho'],['Agosto'],['Setembro'],['Outubro'],['Novembro'],['Dezembro']]
+#Ler arquivo
 while True:
     linha = lerArquivo.readline()
     if linha[-1:] == '\n':
@@ -24,8 +25,7 @@ for data in range(1,13):
     for x in range(len(aux)):
         media += float(aux[data-1][5].replace(',','.'))
     cotaçãomedia[data-1].append(media/len(aux))
-    
-    
+       
 
 for x in range(len(cotaçãomax)):
     print(cotaçãomax[x])
@@ -39,6 +39,7 @@ lerArquivo = open(dirArquivos + '\\CotacoesDolar2024.csv','r',encoding='utf-8')
 arquivo =list()
 cotaçãomedia = [['Janeiro'],['Fevereiro'],['Março'],['Abril'],['Maio'],['Junho'],['Julho'],['Agosto'],['Setembro'],['Outubro'],['Novembro'],['Dezembro']]
 cotaçãomax = [['Janeiro'],['Fevereiro'],['Março'],['Abril'],['Maio'],['Junho'],['Julho'],['Agosto'],['Setembro'],['Outubro'],['Novembro'],['Dezembro']]
+#Ler arquivo
 while True:
     linha = lerArquivo.readline()
     if linha[-1:] == '\n':
@@ -46,7 +47,7 @@ while True:
     if not linha:
         break
     arquivo.append(linha.split(";"))
-
+    
 for data in range(1,9):
     aux = [arquivo[x] for x in range(len(arquivo)) if str(data) + arquivo[x][0][-4] + arquivo[x][0][-3] + arquivo[x][0][-2] + arquivo[x][0][-1] in arquivo[x][0]]
     aux.sort(key=lambda x: x[5], reverse=True)
